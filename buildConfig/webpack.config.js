7a5f8e7546956b5ec05e4727fe756578c4fe2147
genesis-jean <jean.c@gen-game.com>
2020-08-20 22:33:48 +0800

    update output file config

--- buildConfig/webpack.config.js
+++ buildConfig/webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
 	mode: 'development', // 開發模式
 	entry: path.resolve(__dirname, '../src/main.js'),    // 入口文件
 	output: {
-		filename: 'output.js',      // 打包後的文件名稱
+		filename: '[name].[hash:8].js',      // 打包後的文件名稱
 		path: path.resolve(__dirname, '../dist')  // 打包後的目錄
 	}
 }
