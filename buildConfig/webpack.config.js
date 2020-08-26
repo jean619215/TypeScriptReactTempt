7ad6579fe592a0f1281a1b4d0b035a21b61fdcdd
genesis-jean <jean.c@gen-game.com>
2020-08-26 09:45:53 +0800

    add "html-webpack-plugin": "^4.3.0"

--- buildConfig/webpack.config.js
+++ buildConfig/webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
 	mode: 'development', // 開發模式
@@ -6,5 +8,13 @@ module.exports = {
 	output: {
 		filename: '[name].[hash:8].js',      // 打包後的文件名稱
 		path: path.resolve(__dirname, '../dist')  // 打包後的目錄
-	}
+	},
+	plugins: [
+		new CleanWebpackPlugin(),// 清理重複的
+
+		new HtmlWebpackPlugin({
+			template: path.resolve(__dirname, '../public/index.html') // 讀取 輸出的index的模板
+		})
+
+	]
 }
